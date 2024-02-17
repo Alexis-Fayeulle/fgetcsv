0b73a5a61e0810f52b0ca2069a0dc407e83eab44
Alexis Fayeulle <58552341+Alexis-Fayeulle@users.noreply.github.com>
2024-02-17 09:21:43 +0100

    Create testSpeedB.php

---
+++ testSpeedB.php
@@ -0,0 +1,36 @@
+<?php
+
+require_once __DIR__.'/vendor/autoload.php';
+
+$chunk = (int) $argv[1];
+$time_limit = (float) $argv[2];
+
+echo 'chunk: '.$chunk.PHP_EOL;
+echo 'time_limit: '.($time_limit == 0 ? 'Infinite' : $time_limit).PHP_EOL;
+
+$f = fopen(__DIR__.'/very_big_csv.csv', 'rb');
+
+$diff = 0;
+$count = 0;
+$start = microtime(true);
+
+do {
+    for ($i = 0; $i < $chunk; $i++) {
+        $d = myfgetcsv($f);
+        $count++;
+    }
+
+    $diff = microtime(true) - $start;
+
+    if ($time_limit != 0 && $diff > $time_limit) {
+        break;
+    }
+} while ($d !== false);
+
+$stop = microtime(true);
+fclose($f);
+$diff = $stop - $start;
+
+echo 'time: '.$diff.PHP_EOL;
+echo 'count: '.$count.PHP_EOL;
+echo number_format($count / $diff, 0, '', '').' i/s'.PHP_EOL;
